54f8de7fc8c988f4ab984347e67d88c0afe37878
Faraz Loloei <farazloloei@uni-koblenz.de>
2025-06-12 21:23:32 +0200

    ApplicationDbContext is added

---
+++ SampleGraphQLCRUD.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleGraphQLCRUD.API.Models;
+
+namespace SampleGraphQLCRUD.API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Customer> Customers => Set<Customer>();
+
+    public DbSet<Purchase> Purchases => Set<Purchase>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+}
