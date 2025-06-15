6fd74ed7dfbe1289e978d5bffc16759935a5306d
Faraz Loloei <farazloloei@uni-koblenz.de>
2025-06-15 22:21:10 +0200

    Custom Exception Middleware

--- SampleGraphQLCRUD.API/Program.cs
+++ SampleGraphQLCRUD.API/Program.cs
@@ -1,7 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using SampleGraphQLCRUD.API.Abstraction;
 using SampleGraphQLCRUD.API.Data;
-using SampleGraphQLCRUD.API.GraphQL;
+using SampleGraphQLCRUD.API.Extensions.Middleware;
+using SampleGraphQLCRUD.API.GraphQL.Mutations;
 using SampleGraphQLCRUD.API.GraphQL.Queries;
 using SampleGraphQLCRUD.API.GraphQL.Types;
 using SampleGraphQLCRUD.API.Services;
@@ -21,19 +22,22 @@ namespace SampleGraphQLCRUD.API
             builder.Services.AddScoped<ICustomerService, CustomerService>();
             builder.Services.AddScoped<IPurchaseService, PurchaseService>();
 
-            builder.Services
-                .AddGraphQLServer()
+            builder.Services.AddGraphQLServer()
                 .AddQueryType<CustomerQuery>()
                 .AddQueryType<PurchaseQuery>()
-                .AddMutationType<Mutation>()
+                .AddMutationType<CustomerMutation>()
+                .AddMutationType<PurchaseMutation>()
                 .AddType<CustomerType>()
                 .AddType<PurchaseType>()
+                .AddErrorFilter<ErrorFilter>()
+                .UseField<CustomExceptionMiddleware>()
                 .AddProjections()
                 .AddFiltering()
                 .AddSorting();
 
             var app = builder.Build();
 
+            app.MapGraphQL(); // Endpoint: /graphql
             app.Run();
         }
     }
